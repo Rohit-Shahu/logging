c3e55f21e963015a40a9f4f383f3f42be6bf8c56
Rohit-Shahu <rohitsatyasai2000@gmail.com>
2020-07-20 18:16:15 +0530

    Initial Commit : Logging

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+class Construction
+{
+
+
+    public static Logger LOGGER= LogManager.getLogger(Construction.class);
+    int standard,house_area;
+    Construction(int std,int area)
+    {
+        standard=std;
+        house_area=area;
+    }
+    void calculateCost()
+    {
+        if(standard==1) {
+            LOGGER.debug("LOW STANDARD CHOSEN");
+            System.out.println("Construction Cost: "+house_area*12000);
+        }
+        else if(standard==2) {
+            LOGGER.debug("ABOVE STANDARD CHOSEN");
+            System.out.println("Construction Cost: "+house_area*15000);
+        }
+        else if(standard==3) {
+            LOGGER.debug("HIGH STANDARD CHOSEN");
+            System.out.println("Construction Cost:"+house_area*18000);
+        }
+        else {
+            LOGGER.debug("HIGH STANDARD AND FULLY AUTOMATED CHOSEN");
+            System.out.println("Construction Cost:"+house_area*25000);
+        }
+    }
+}
